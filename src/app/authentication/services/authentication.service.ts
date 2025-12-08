ab9c43ddd403816195f635a24325e6f9d660aadd
oscar <oscarcastellote1@gmail.com>
2025-12-08 19:02:05 +0100

    -Changed color-primary-a.
    -Added icons from Figma to the assets folder.
    
    -Created navbar component (initial design).

--- src/app/authentication/services/authentication.service.ts
+++ src/app/authentication/services/authentication.service.ts
@@ -12,7 +12,7 @@ export class AuthenticationService {
     private http = inject(HttpClient);
     private readonly API_URL: string = environment.baseUrl;
 
-    private _user = signal<User | null>(null);
+    private _user = signal<User | null>(this.getStoredUser());
     private _token = signal<string | null>(localStorage.getItem('token'));
 
     // Getters
@@ -20,12 +20,18 @@ export class AuthenticationService {
     token = computed(() => this._token());
     isAuthenticated = computed(() => !!this._token());
 
+    private getStoredUser(): User | null {
+        const user: string | null = localStorage.getItem('user');
+        return user !== null ? JSON.parse(user) as User : null;
+    }
+
     login(username: string, password: string): Observable<boolean> {
         return this.http.post<AuthResponse>(`${this.API_URL}/login`, {username, password}).pipe(
             map((res: AuthResponse) => {
                 this._user.set(res.user);
                 this._token.set(res.securityKey);
                 localStorage.setItem('token', res.securityKey);
+                localStorage.setItem('user', JSON.stringify(res.user));
                 return true;
             }),
             catchError(() => of(false))
@@ -35,6 +41,8 @@ export class AuthenticationService {
     logout(): void {
         this._user.set(null);
         this._token.set(null);
+
         localStorage.removeItem('token');
+        localStorage.removeItem('user');
     }
 }
--- src/app/shared/components/navbar-component/navbar.component.ts
+++ src/app/shared/components/navbar-component/navbar.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { MatToolbarModule } from "@angular/material/toolbar";
+import {Component, inject} from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {Router} from '@angular/router';
+import {AuthenticationService} from '../../../authentication/services/authentication.service';
+import {Location} from '@angular/common';
+import {MatIconButton} from '@angular/material/button';
 
 @Component({
-  selector: 'app-navbar-component',
-  standalone: true,
-  imports: [MatToolbarModule],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+    selector: 'app-navbar-component',
+    standalone: true,
+    imports: [MatToolbarModule, MatIconButton],
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+    authService = inject(AuthenticationService);
+    router = inject(Router);
+    location = inject(Location);
 
+    logout(): void {
+        this.authService.logout();
+        this.router.navigateByUrl('/login');
+    }
+
+    navigateBack(): void {
+        this.location.back();
+    }
 }
