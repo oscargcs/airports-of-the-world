1fe05c5221fcaf50e019692cef4fe6571faef243
oscar <oscarcastellote1@gmail.com>
2025-12-10 15:27:18 +0100

    -Moved side-menu to main-layout.component
    -Simplified main-layout component html and scss.

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {FooterComponent} from '../../shared/components/footer/footer.component';
 import {NavbarComponent} from '../../shared/components/navbar-component/navbar.component';
 import {RouterOutlet} from '@angular/router';
+import {SideMenuComponent} from '../../shared/components/side-menu/side-menu.component';
 
 @Component({
     selector: 'app-main-layout',
@@ -9,7 +10,8 @@ import {RouterOutlet} from '@angular/router';
     imports: [
         FooterComponent,
         NavbarComponent,
-        RouterOutlet
+        RouterOutlet,
+        SideMenuComponent
     ],
     templateUrl: './main-layout.component.html',
     styleUrl: './main-layout.component.scss'
