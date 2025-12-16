9ebd0a334b9882f2962ca22f8e28946e6fbc78e4
oscar <oscarcastellote1@gmail.com>
2025-12-16 11:32:37 +0100

    -Moved textLinks and iconLinks to main-layout.component

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,8 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {FooterComponent} from '../../shared/components/footer/footer.component';
 import {NavbarComponent} from '../../shared/components/navbar-component/navbar.component';
 import {RouterOutlet} from '@angular/router';
 import {SideMenuComponent} from '../../shared/components/side-menu/side-menu.component';
+import {ClickableTextLink} from '../../shared/components/types/clicable-text-link';
+import {SocialPlatformName} from '../../shared/components/types/social-platform-name';
 
 @Component({
     selector: 'app-main-layout',
@@ -16,5 +18,16 @@ import {SideMenuComponent} from '../../shared/components/side-menu/side-menu.com
     styleUrl: './main-layout.component.scss'
 })
 export default class MainLayoutComponent {
+    public textLinks = signal<ClickableTextLink[]>([
+        {
+            label: 'About us',
+            href: 'https://www.vueling.com/en/about-us'
+        },
+        {
+            label: 'Help',
+            href: 'https://www.vueling.com/en/we-are-vueling/contact'
+        }
+    ]);
 
+    public iconLinks = signal<SocialPlatformName[]>(['twitter', 'facebook', 'instagram', 'youtube']);
 }
--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import {Component, signal} from '@angular/core';
+import {Component, input} from '@angular/core';
 import {SocialLinkComponent} from '../../ui/clickable-icon/social-link.component';
 import {SocialPlatformName} from '../types/social-platform-name';
 import {ClickableTextLink} from '../types/clicable-text-link';
@@ -14,16 +14,7 @@ import {TextLinkComponent} from '../../ui/text-link/text-link.component';
     styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-    public textLinks = signal<ClickableTextLink[]>([
-        {
-            label: 'About us',
-            href: 'https://www.vueling.com/en/about-us'
-        },
-        {
-            label: 'Help',
-            href: 'https://www.vueling.com/en/we-are-vueling/contact'
-        }
-    ]);
+    public textLinks = input<ClickableTextLink[]>();
 
-    public iconLinks = signal<SocialPlatformName[]>(['twitter', 'facebook', 'instagram', 'youtube']);
+    public iconLinks = input<SocialPlatformName[]>();
 }
